93311c03da694848581ad79ed7c0cbd4d9660cbf
Marcus-Giglio <gigliomarcus9@gmail.com>
2022-09-16 15:20:57 +0200

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Button, Card, Col, Container, Row } from "react-bootstrap";
-import illustrationHero from "./assets/illustration-hero.svg";
 import iconMusic from "./assets/icon-music.svg";
 import CardHeader from "react-bootstrap/esm/CardHeader";
+import illustrationHero from "./assets/illustration-hero.svg";
 
 function App() {
   return (
@@ -11,40 +11,40 @@ function App() {
       <Container>
         <Row>
           <Col md={7} lg={5} xl={4} className="mx-auto col-12">
-            <Card>
-              <CardHeader>
-                <Card.Img src={illustrationHero} className="img-fluid" />
-              </CardHeader>
-              <Card.Body className="px-5 py-4">
-                <Card.Title className="m-0 pt-2">Order Summary</Card.Title>
-                <Card.Text className="m-0 pb-4 pt-3 px-3">
-                  You can now listen to millions of songs, audiobooks, and
-                  podcasts on any device anywhere you like!
-                </Card.Text>
-                <div className="info my-1 p-3">
-                  <div>
+            <main>
+              <Card>
+                <CardHeader>
+                  <Card.Img src={illustrationHero} className="img-fluid" />
+                </CardHeader>
+                <Card.Body className="px-5 py-4">
+                  <Card.Title className="m-0 pt-2">Order Summary</Card.Title>
+                  <Card.Text className="m-0 pb-4 pt-3 px-3">
+                    You can now listen to millions of songs, audiobooks, and
+                    podcasts on any device anywhere you like!
+                  </Card.Text>
+                  <div className="info my-1 p-3">
                     <img src={iconMusic} className="img-fluid" />
+                    <div>
+                      <p className="m-0 card-title">Annual Plan</p>
+                      <p className="m-0 card-text">$59.99/year</p>
+                    </div>
+                    <div>
+                      <button className="change-btn">Change</button>
+                    </div>
                   </div>
-                  <div>
-                    <p className="m-0 card-title">Annual Plan</p>
-                    <p className="m-0 card-text">$59.99/year</p>
-                  </div>
-                  <div>
-                    <button className="change-btn">Change</button>
-                  </div>
-                </div>
-                <Row className="pt-4">
-                  <Col sm={12} className="mb-3 text-center">
-                    <Button className="payment-btn w-100 p-3">
-                      Proceed to Payment
-                    </Button>
-                  </Col>
-                  <Col sm={12} className="text-center">
-                    <Button className="cancel-btn">Cancel Order</Button>
-                  </Col>
-                </Row>
-              </Card.Body>
-            </Card>
+                  <Row className="pt-4">
+                    <Col sm={12} className="mb-3 text-center">
+                      <Button className="payment-btn w-100 p-3">
+                        Proceed to Payment
+                      </Button>
+                    </Col>
+                    <Col sm={12} className="text-center">
+                      <Button className="cancel-btn">Cancel Order</Button>
+                    </Col>
+                  </Row>
+                </Card.Body>
+              </Card>
+            </main>
           </Col>
         </Row>
       </Container>
